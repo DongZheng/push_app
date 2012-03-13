faf889372810af7f9af79f5365b63ffeb573ae20
Zheng Dong <a23211@motorola.com>
2012-03-13 15:31:21 +0800

    Initial commit for 'rails new push_app -T'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PushApp::Application.config.secret_token = 'fd4ff090c41f03d9cedc42eaa49b01ee194f7c7b806c432a919a0d039c11ee75fa799eda0c995b131fb56dc1025d5b298d2bfe50d2ccc47f94f9969958ea9064'
