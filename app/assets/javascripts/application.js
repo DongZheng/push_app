615a5e691b706e3d2a29d39b6d0d084e61d338cd
Zheng Dong <a23211@motorola.com>
2012-03-13 16:22:32 +0800

    Leverage the faye server. To start faye server: $ rackup faye.ru -s thin -E production Note that the gem 'thin' must be installed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(function() {
+	var faye = new Faye.Client('http://localhost:9292/faye');
+	faye.subscribe("/messages/new", function(data) {
+		eval(data);
+	});
+});
